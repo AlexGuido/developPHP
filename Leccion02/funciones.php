c89fc1e375d1902235b2aba5159b716f53ba22ae
AlexGuido <alexanderguido@outlook.com>
2022-10-06 12:51:16 -0500

    Se crea la leccion 2

---
+++ Leccion02/funciones.php
@@ -0,0 +1,42 @@
+<?php
+
+/*Funcion: conjunto de instrucciones agrupadas bajo un nombre 
+en concreto y pueden reutilizarse solamente invocando a la 
+funcion 
+*/
+//Ejemplo 1
+function showNames(){
+    echo "www.alexguido.com <br>";
+    echo "www.alxguido.com <br>";
+}
+
+showNames();
+//Ejemplo 2
+function multi($x){
+    echo "<h3>Tabla de multiplicar del $x</h3>";
+    for($i=1; $i<=10; $i++){
+        echo "$x x $i = ".($i*$x)."<br>";
+    }
+}
+//pedir el numero por url
+if(isset($_GET['x'])){
+    multi($_GET['x']);
+}else{
+    echo "Error";
+}
+//ejemplo 3 (funcion con dos parametros)
+function calc($x,$y){
+    echo "<h4>Operaciones Basicas con $x y $y</h4>";
+    echo "Suma = ".($x+$y)."<br>";
+    echo "Resta = ".($x-$y)."<br>";
+    echo "Multiplicacion = ".($x*$y)."<br>";
+    echo "Division = ".($x/$y)."<br>";
+}
+//pedir el numero por url
+if(isset($_GET['x'],$_GET['y'])){
+    calc($_GET['x'],$_GET['y']);
+}else{
+    echo "Error";
+}
+
+?>
